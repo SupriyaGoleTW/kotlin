a0468aa577fab9ae32d9386348e6d7f868c0e1a9
Supriya <supriyag@thoughtworks.com>
2019-01-07 15:31:22 +0530

    Supriya | Adding function to return sum of given numbers in array.

---
+++ src/test/com/example/addition/additionTests.kt
@@ -0,0 +1,11 @@
+import main.com.example.addition.addAllNumbers
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+class AdditionTest {
+    @Test
+    fun testReturnsAdditionOfGivenNumbers(){
+        assertEquals(150, addAllNumbers(arrayOf(10, 20, 30, 40, 50)))
+        assertEquals(0, addAllNumbers(arrayOf()))
+        assertEquals(0, addAllNumbers(arrayOf(-10, 10)))
+    }
+}
